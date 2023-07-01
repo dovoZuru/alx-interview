6bb5a8ac5c0659fb604aaaeeb6070f41c390e5f1
dovoZuru <dovo.fav@aol.com>
2023-07-01 10:17:29 +0000

    Pascal's triangle virtual interview

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+0: Pascal's Triangle
+'''
+
+def pascal_triangle(n):
+    '''
+    Returns a list of lists of integers representing the Pascal's triangle of n.
+    '''
+    if n <= 0:
+        return []
+    result = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(result[i-1][j-1] + result[i-1][j])
+        row.append(1)
+        result.append(row)
+    return result
